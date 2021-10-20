46674b563f50e3b1897b0ca4d1f88393374abeb1
Joshua Cerdenia <joshcerdenia@gmail.com>
2021-10-20 22:41:39 +0800

    Add routes to automatically direct to articles page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,11 @@ import { ContactComponent } from './contact/contact.component';
 const routes: Routes = [
   // When the URL points to path, activate and display component
   { path: 'contact', component: ContactComponent },
-  { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) }
+  { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) },
+  { path: '', pathMatch: 'full', redirectTo: 'articles' },
+  { path: '**', redirectTo: 'articles' }, 
+  // ** is wild card, triggered when router cannot match requested URL with a defined route
+  // NB all these routes are parsed in order, first match wins
 ];
 
 @NgModule({
