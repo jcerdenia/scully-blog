29ec0a16a0c51a04fc57c23d8e33f3742e15f7f8
Joshua Cerdenia <joshcerdenia@gmail.com>
2021-10-20 22:16:25 +0800

    Create header and footer components and display them on screen

---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+// This component displays the main links of the blog
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
